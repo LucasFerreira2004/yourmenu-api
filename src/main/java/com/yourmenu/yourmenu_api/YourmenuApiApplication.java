f5f9fe3759959f0c8bebb7b87b877ac6eb49a506
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-04-23 21:02:52 -0300

    first commit

---
+++ src/main/java/com/yourmenu/yourmenu_api/YourmenuApiApplication.java
@@ -0,0 +1,13 @@
+package com.yourmenu.yourmenu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourmenuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourmenuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourmenu/yourmenu_api/YourmenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourmenu.yourmenu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourmenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
