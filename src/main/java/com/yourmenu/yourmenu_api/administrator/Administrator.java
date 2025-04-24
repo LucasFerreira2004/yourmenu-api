ee8e59fffd53352ea7207d4cad3e56f319631e44
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-04-23 23:34:15 -0300

    back-feat: administrator

---
+++ src/main/java/com/yourmenu/yourmenu_api/administrator/Administrator.java
@@ -0,0 +1,62 @@
+package com.yourmenu.yourmenu_api.administrator;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data //do lombok
+public class Administrator extends UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String password;
+
+
+    //userDetails
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/yourmenu/yourmenu_api/config/EnvConfig.java
+++ src/main/java/com/yourmenu/yourmenu_api/config/EnvConfig.java
@@ -1,4 +1,13 @@
 package com.yourmenu.yourmenu_api.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class EnvConfig {
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.load();
+    }
 }
