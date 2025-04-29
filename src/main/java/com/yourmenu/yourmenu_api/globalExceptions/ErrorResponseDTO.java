e6a649718977b4d5fe699aeb44c1813f6d999389
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-04-29 16:02:31 -0300

    feat: GlobalExceptionHandler e ErrorResponseDTO

---
+++ src/main/java/com/yourmenu/yourmenu_api/globalExceptions/ErrorResponseDTO.java
@@ -0,0 +1,4 @@
+package com.yourmenu.yourmenu_api.globalExceptions;
+
+public record ErrorResponseDTO(String field, String message) {
+}
---
+++ src/main/java/com/yourmenu/yourmenu_api/globalExceptions/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.yourmenu.yourmenu_api.globalExceptions;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
