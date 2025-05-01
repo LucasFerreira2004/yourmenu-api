65a5b5d5f37c0f7233ee1299c756e483a0a1a913
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-05-01 13:34:30 -0300

    infra: adicionando nova dependencia para ler .env e removendo EnvConfig

--- src/main/java/com/yourmenu/yourmenu_api/YourmenuApiApplication.java
+++ src/main/java/com/yourmenu/yourmenu_api/YourmenuApiApplication.java
@@ -1,5 +1,6 @@
 package com.yourmenu.yourmenu_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YourmenuApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // carrega o .env
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(YourmenuApiApplication.class, args);
 	}
 
