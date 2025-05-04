82811056179ff6dc553c301f2664fea54a1ebbb4
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-05-04 16:08:13 -0300

    feat: RestaurantSlugService

--- src/main/java/com/yourmenu/yourmenu_api/restaurant/RestaurantSlugService.java
+++ src/main/java/com/yourmenu/yourmenu_api/restaurant/RestaurantSlugService.java
@@ -1,4 +1,23 @@
-package com.yourmenu.yourmenu_api.shared.services;
+package com.yourmenu.yourmenu_api.restaurant;
 
-public class SlugService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestaurantSlugService {
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public String generateSlug(String restaurantName) {
+
+    }
+    public boolean isFree(String slug) {
+        return restaurantRepository.findBySlug(slug) == null;
+    }
+
+    public String slugFormater(String restaurantName) {
+        restaurantName = restaurantName.toLowerCase();
+        restaurantName = restaurantName.replaceAll(" ", "-");
+        return restaurantName;
+    }
 }
