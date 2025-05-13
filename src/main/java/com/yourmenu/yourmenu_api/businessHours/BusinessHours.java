50631f2b96d8169729d615169e8ad0373be4fb38
kauan <kauancavalcante8p@gmail.com>
2025-05-12 21:59:59 -0300

    adicionando comportamentos de instanciação com valor nulo para algumas colunas

--- src/main/java/com/yourmenu/yourmenu_api/businessHours/BusinessHours.java
+++ src/main/java/com/yourmenu/yourmenu_api/businessHours/BusinessHours.java
@@ -3,12 +3,14 @@ package com.yourmenu.yourmenu_api.businessHours;
 import com.yourmenu.yourmenu_api.restaurant.Restaurant;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 import java.util.UUID;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class BusinessHours {
 
     @Id
@@ -22,7 +24,11 @@ public class BusinessHours {
     @Enumerated(EnumType.STRING)
     private Weekday weekday;
 
-    private LocalTime openingTime;
+    @Column(name = "opening_time", nullable = true)
+    private LocalTime openingTime = null;
 
-    private LocalTime closingTime;
+    @Column(name = "closing_time", nullable = true)
+    private LocalTime closingTime = null;
+
+    private Boolean isOpen = false;
 }
