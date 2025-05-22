fb1a641e026eb93def0f1e37dfe3ad6aa0818f3f
kauan <kauancavalcante8p@gmail.com>
2025-05-21 21:19:37 -0300

    createdBusinessUseCase para createdBusinessService

--- src/main/java/com/yourmenu/yourmenu_api/businessHours/services/CreateBusinessHoursService.java
+++ src/main/java/com/yourmenu/yourmenu_api/businessHours/services/CreateBusinessHoursService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CreateBusinessHoursUseCase {
+public class CreateBusinessHoursService {
 
     @Autowired
     BusinessHoursRepository businessHoursRepository;
