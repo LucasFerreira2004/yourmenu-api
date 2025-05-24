08de2d14079395c6c4c0c55f6efc76bfa965b08c
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-05-24 19:48:38 -0300

    feat: add exception categoryNotFound e exceptionHandler

---
+++ src/main/java/com/yourmenu/yourmenu_api/category/Exceptions/CategoryNotFoundException.java
@@ -0,0 +1,12 @@
+package com.yourmenu.yourmenu_api.category.Exceptions;
+
+import com.yourmenu.yourmenu_api.shared.globalExceptions.ApplicationException;
+
+public class CategoryNotFoundException extends ApplicationException {
+    public CategoryNotFoundException(String field, String message) {
+        super(field, message);
+    }
+    public CategoryNotFoundException(String message) {super("categoryId", message);}
+    public CategoryNotFoundException() {super("categoryId", "categoria nao encontrada");}
+
+}
