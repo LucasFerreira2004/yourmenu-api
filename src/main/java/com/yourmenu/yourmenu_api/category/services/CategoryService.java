ce7e097046fcecb107d2783c923f2893319cb5ba
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-05-24 21:53:15 -0300

    feat: add validacoes em save category

--- src/main/java/com/yourmenu/yourmenu_api/category/services/CategoryService.java
+++ src/main/java/com/yourmenu/yourmenu_api/category/services/CategoryService.java
@@ -5,6 +5,7 @@ import com.yourmenu.yourmenu_api.category.CategoryRepository;
 import com.yourmenu.yourmenu_api.category.dto.CategoryDTO;
 import com.yourmenu.yourmenu_api.category.dto.CategorySaveDTO;
 import com.yourmenu.yourmenu_api.category.mapper.CategoryMapper;
+import com.yourmenu.yourmenu_api.category.validation.CategoryValidateService;
 import com.yourmenu.yourmenu_api.restaurant.Restaurant;
 import com.yourmenu.yourmenu_api.restaurant.RestaurantRepository;
 import com.yourmenu.yourmenu_api.restaurant.RestaurantService;
@@ -15,17 +16,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryService {
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private RestaurantRepository restaurantRepository;
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private CategoryValidateService categoryValidateService;
 
     public CategoryDTO save(CategorySaveDTO dto, String restaurantId, String adminId){
-        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(()-> new RestaurantNotFoundException("restaurantId"));
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(()-> new RestaurantNotFoundException());
         Category category = CategoryMapper.toEntity(dto, restaurant);
+        categoryValidateService.validateAdminCanSaveCategory(category, adminId);
         categoryRepository.save(category);
         return CategoryMapper.toDto(category);
     }
+
     public CategoryDTO update(CategorySaveDTO dto, String AdminId){
         return null;
     }
