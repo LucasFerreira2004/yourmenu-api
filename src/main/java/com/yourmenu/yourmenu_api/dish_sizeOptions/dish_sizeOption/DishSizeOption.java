3cf935f0acba052a57f1c00db6ff4c173a2bdbf2
kauan <kauancavalcante8p@gmail.com>
2025-06-09 23:09:57 -0300

    Garantindo que price seja armazenado sempre com duas casas decimais e maior que zero

--- src/main/java/com/yourmenu/yourmenu_api/dish_sizeOptions/dish_sizeOption/DishSizeOption.java
+++ src/main/java/com/yourmenu/yourmenu_api/dish_sizeOptions/dish_sizeOption/DishSizeOption.java
@@ -2,12 +2,14 @@ package com.yourmenu.yourmenu_api.dish_sizeOptions.dish_sizeOption;
 
 import com.yourmenu.yourmenu_api.dish_sizeOptions.sizeOptions.SizeOption;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Entity
 @NoArgsConstructor
@@ -22,7 +24,17 @@ public class DishSizeOption {
     @JoinColumn(name = "sizeOption_id", nullable = false)
     private SizeOption sizeOption;
 
+    @Column(precision = 10, scale = 2)
+    @Digits(integer = 8, fraction = 2)
     @NotNull
-    @Min(0)
-    private BigDecimal value;
+    @DecimalMin("0.0")
+    private BigDecimal price;
+
+    public void setPrice(BigDecimal price) {
+        if (price != null) {
+            this.price = price.setScale(2, RoundingMode.HALF_UP);
+        } else {
+            this.price = null;
+        }
+    }
 }
