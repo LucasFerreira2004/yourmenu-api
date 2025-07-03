4edaf692720b38dc7240576550edb55c6c9e145b
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-07-03 11:16:09 -0300

    fix: envio dos dtos de deliveryZone e order no dto de orderAdress

--- src/main/java/com/yourmenu/yourmenu_api/orderAdress/dto/OrderAdressDto.java
+++ src/main/java/com/yourmenu/yourmenu_api/orderAdress/dto/OrderAdressDto.java
@@ -1,12 +1,14 @@
 package com.yourmenu.yourmenu_api.orderAdress.dto;
 
 import com.yourmenu.yourmenu_api.deliveryZone.DeliveryZone;
+import com.yourmenu.yourmenu_api.deliveryZone.dto.DeliveryZoneDto;
 import com.yourmenu.yourmenu_api.order.Order;
+import com.yourmenu.yourmenu_api.order.dto.OrderDTO;
 
 public record OrderAdressDto(
         Long id,
-        Order order,
-        DeliveryZone deliveryZone,
+        OrderDTO order,
+        DeliveryZoneDto deliveryZone,
         Long cep,
         String street,
         String number,
