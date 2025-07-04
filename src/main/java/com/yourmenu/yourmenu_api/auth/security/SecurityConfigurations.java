fb98a937d7646ea116a9ec048d77fa5c7d36ab40
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-07-04 18:54:25 -0300

    refac: permitindo livre acesso as rotas de get de OrderController

--- src/main/java/com/yourmenu/yourmenu_api/auth/security/SecurityConfigurations.java
+++ src/main/java/com/yourmenu/yourmenu_api/auth/security/SecurityConfigurations.java
@@ -32,6 +32,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/restaurant/*/order").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/restaurant/*/order").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
