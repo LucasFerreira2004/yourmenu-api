816ae94a8cea0c850cacf2e4baf7f91556a192d4
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-07-07 10:18:35 -0300

    refac: recebendo apenas nome em OrderClientSaveDTO

--- src/main/java/com/yourmenu/yourmenu_api/order_client/dto/OrderClientSaveDTO.java
+++ src/main/java/com/yourmenu/yourmenu_api/order_client/dto/OrderClientSaveDTO.java
@@ -6,13 +6,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record OrderClientSaveDTO(
-        @NotBlank(message = "O primeiro nome é obrigatório")
-        @Size(min = 2, max = 50, message = "O primeiro nome deve ter entre 2 e 50 caracteres")
-        String firstName,
-
-        @NotBlank(message = "O sobrenome é obrigatório")
-        @Size(min = 2, max = 50, message = "O sobrenome deve ter entre 2 e 50 caracteres")
-        String lastName,
+        @NotBlank(message = "O nome nome é obrigatório")
+        @Size(min = 2, max = 255, message = "O nome deve ter entre 2 e 255 caracteres")
+        String name,
 
         @NotNull(message = "O telefone é obrigatório")
         @Digits(integer = 11, fraction = 0, message = "O telefone deve ter até 11 dígitos e sem casas decimais")
