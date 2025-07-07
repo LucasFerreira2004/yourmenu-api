976f705acf316e8326ad404de931339a80672c32
kauan <kauancavalcante8p@gmail.com>
2025-07-06 23:42:05 -0300

    adicionando DTO de Client no DTO de ordersave

--- src/main/java/com/yourmenu/yourmenu_api/order_client/OrderClientService.java
+++ src/main/java/com/yourmenu/yourmenu_api/order_client/OrderClientService.java
@@ -1,7 +1,19 @@
 package com.yourmenu.yourmenu_api.order_client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderClientService {
+
+    @Autowired
+    private OrderClientRepository orderClientRepository;
+
+    @Autowired
+    private OrderClientMapper orderClientMapper;
+
+    public void saveOrderClinet(OrderClient orderClient) {
+
+    }
+
 }
