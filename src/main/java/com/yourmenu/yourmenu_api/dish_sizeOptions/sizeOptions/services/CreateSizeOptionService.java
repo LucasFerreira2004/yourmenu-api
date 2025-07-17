9f1adbb5617d6ae702f6b07cdd25181603394261
kauan <kauancavalcante8p@gmail.com>
2025-07-17 02:37:04 -0300

    fix: adiciando valores mais reais de SizeOption

--- src/main/java/com/yourmenu/yourmenu_api/dish_sizeOptions/sizeOptions/services/CreateSizeOptionService.java
+++ src/main/java/com/yourmenu/yourmenu_api/dish_sizeOptions/sizeOptions/services/CreateSizeOptionService.java
@@ -1,6 +1,5 @@
 package com.yourmenu.yourmenu_api.dish_sizeOptions.sizeOptions.services;
 
-import com.yourmenu.yourmenu_api.dish_sizeOptions.sizeOptions.mappers.SizeOptionsMapper;
 import com.yourmenu.yourmenu_api.dish_sizeOptions.sizeOptions.MeasureUnit;
 import com.yourmenu.yourmenu_api.dish_sizeOptions.sizeOptions.SizeOption;
 import com.yourmenu.yourmenu_api.dish_sizeOptions.sizeOptions.SizeOptionRepository;
@@ -8,33 +7,41 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class CreateSizeOptionService {
 
     @Autowired
     private SizeOptionRepository sizeOptionRepository;
 
-    @Autowired
-    private SizeOptionsMapper sizeOptionsMapper;
-
-    //DEIXAR PRONTA PARA INICIALIZAR COM TODOS OS TAMANHOS CORRETAMENTE - PERGUNTAR QUAIS TAMANHOS ADICIONAR
     @PostConstruct
     private void init() {
-        if (sizeOptionRepository.count() > 0) return; // Evita duplicar ao reiniciar
+        if (sizeOptionRepository.count() > 0) return;
 
-        for (int i = 0; i < MeasureUnit.values().length; i++) {
-            if (i < 3) {
+        // Tamanhos simbólicos: PEQUENO, MEDIO, GRANDE
+        for (int i = 0; i < 3; i++) {
+            SizeOption sizeOption = new SizeOption();
+            sizeOption.setMeasureUnit(MeasureUnit.values()[i]);
+            sizeOption.setMagnitude(null);
+            sizeOptionRepository.save(sizeOption);
+        }
+
+        // Tamanhos com magnitude realistas
+        Map<MeasureUnit, List<String>> medidasComuns = Map.of(
+                MeasureUnit.MILILITRO, List.of("200", "300", "350", "500", "600", "1000"),
+                MeasureUnit.LITRE, List.of("1", "1.5", "2", "5"),
+                MeasureUnit.GRAMA, List.of("50", "100", "150", "200", "300", "500"),
+                MeasureUnit.KILOGRAMA, List.of("0.5", "1", "1.5", "2")
+        );
+
+        for (Map.Entry<MeasureUnit, List<String>> entry : medidasComuns.entrySet()) {
+            MeasureUnit unidade = entry.getKey();
+            for (String magnitude : entry.getValue()) {
                 SizeOption sizeOption = new SizeOption();
-                sizeOption.setMeasureUnit(MeasureUnit.values()[i]);
-                sizeOption.setMagnitude(null);
-                sizeOptionRepository.save(sizeOption);
-                continue;
-            }
-            for (int j = 0; j < 4; j++) {
-                SizeOption sizeOption = new SizeOption();
-                int mag = 50 * (j + 1);
-                sizeOption.setMeasureUnit(MeasureUnit.values()[i]);
-                sizeOption.setMagnitude(String.valueOf(mag));
+                sizeOption.setMeasureUnit(unidade);
+                sizeOption.setMagnitude(magnitude);
                 sizeOptionRepository.save(sizeOption);
             }
         }
